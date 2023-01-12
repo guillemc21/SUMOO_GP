3dfdb06de0b6fee35a673aa4848b44853d66746b
Byguillemc21 <57747085+Byguillemc21@users.noreply.github.com>
2023-01-12 15:49:30 +0100

    yes
    yes

--- sumogp/routes/web.php
+++ sumogp/routes/web.php
@@ -19,9 +19,9 @@ Route::get('/', function () {
 });
 
 
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
