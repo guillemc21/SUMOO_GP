f2f4c462c69166b90eb8bd26693f596328a9ef78
Byguillemc21 <57747085+Byguillemc21@users.noreply.github.com>
2023-01-18 16:41:43 +0100

    donde esta sturridge
    donde esta sturridge

--- sumogp/resources/views/layouts/layout.blade.php
+++ sumogp/resources/views/layouts/layout.blade.php
@@ -6,27 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="css/styles.css">
 </head>
-<body>
-     <!-- Logo -->
-     <nav class="navbar navbar-light bg-main">
+<body> 
+    <!-- Logo -->
+    <nav class="navbar navbar-light bg-main">
         <div class="container p-4">
             <a class="navbar-brand m-auto" href="#">
-                <img src="{{asset('images/logo.png')}}" width="120" alt="" loading="lazy">
+                <img src="{{asset('images/sumo.png')}}" width="120" alt="" loading="lazy">
             </a>
         </div>
     </nav>
-    
+
     <!-- Contenido -->
     @yield('content')
-
+   
     <!-- Footer -->
+
     <footer class="container-fluid bg-main">
         <div class="row text-center p-4">
             <div class="mb-3">
-                <img src="{{asset('images/logo.png')}}" alt="YouDevs logo" width="120" id="logofooter">
+                <img src="{{asset('images/sumo.png')}}" alt="YouDevs logo" width="120" id="logofooter">
             </div>
             <div id="col-md-10">
                 <a href="https://www.facebook.com/youdevs">
@@ -43,6 +46,7 @@
         </div>
     </footer>
 
+    <!-- JS -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
