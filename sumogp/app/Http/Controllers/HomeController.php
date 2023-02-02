b463e212588f005db49e2012b50609b74650426d
Byguillemc21 <57747085+Byguillemc21@users.noreply.github.com>
2023-02-02 18:27:45 +0100

    tt
    tt

--- sumogp/app/Http/Controllers/HomeController.php
+++ sumogp/app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $category=Category::find(6);
+        // dd($category->posts);
         return view('posts');
     }
 
--- sumogp/app/Models/Post.php
+++ sumogp/app/Models/Post.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Post extends Model
 {
-    public function posts(){
-        return $this->hasMany('App\Models\Post');
+    public function category(){
+        return $this->belongsTo('App\Models\Category');
     }
 }
