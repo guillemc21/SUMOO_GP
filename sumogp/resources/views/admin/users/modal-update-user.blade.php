d2d0c408fedf1940269e2d6c02a7360b95868904
Byguillemc21 <57747085+Byguillemc21@users.noreply.github.com>
2023-03-20 16:43:28 +0100

    A12
    fds

--- sumogp/resources/views/admin/users/modal-update-user.blade.php
+++ sumogp/resources/views/admin/users/modal-update-user.blade.php
@@ -23,7 +23,7 @@
                     </div>
                     <div class="form-group">
                         <label for="password">Contrasenya</label> 
-                        <input type="text" name="passw0rd" class="form-control" id="password">
+                        <input type="text" name="password" class="form-control" id="password">
                     </div>
                     <div class="form-group">
                         <label for="role">Role</label> 
