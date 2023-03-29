acf7018b4972c23f52e8a94b1a6bad8eddd79638
paumotos <pmotos@insdanielblanxart.cat>
2023-03-29 14:31:45 +0200

    Creacion tabla factura

--- sumogp/app/Models/User.php
+++ sumogp/app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function factura(){
+        return $this->belongsTo('App\Models\Factura');
+    }
+
 }
