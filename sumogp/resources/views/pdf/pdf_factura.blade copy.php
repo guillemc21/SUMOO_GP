901c4541f7c99d428a867a15c658218e43a8b52c
paumotos <pmotos@insdanielblanxart.cat>
2023-05-01 17:52:12 +0200

    css pdf

---
+++ sumogp/resources/views/pdf/pdf_factura.blade copy.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Factura de {{ $nameuser }}</title>
+     <style>
+      /* Estilos para el encabezado */
+      header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #444;
+        color: white;
+        padding: 1rem;
+      }
+
+      /* Estilos para la información del emisor */
+      .emisor {
+        margin-top: 2rem;
+        margin-bottom: 1rem;
+        display: flex;
+        flex-direction: column;
+        font-weight: bold;
+      }
+
+      /* Estilos para la información del receptor */
+      .receptor {
+        margin-bottom: 1rem;
+        display: flex;
+        flex-direction: column;
+        font-weight: bold;
+      }
+
+      /* Estilos para la tabla de productos */
+      table {
+        border-collapse: collapse;
+        width: 100%;
+        margin-bottom: 2rem;
+      }
+
+      th, td {
+        border: 1px solid black;
+        padding: 0.5rem;
+        text-align: center;
+      }
+
+      th {
+        background-color: #444;
+        color: white;
+      }
+
+      /* Estilos para el total */
+      .total {
+        display: flex;
+        justify-content: flex-end;
+        font-weight: bold;
+      }
+    </style>
+</head>
+<body>
+    <!-- @if($content != null)
+      <h1>prueba</h1>
+    @endif -->
+    <header>
+      <h1>Factura</h1>
+      <p>Fecha: {{ $date }}</p>
+    </header>
+
+    <div class="emisor">
+      <h2>Información del emisor</h2>
+      <p>Nombre: {{ $nameuser }}</p>
+      <p>Apellidos: {{ $last_name }}</p>
+      <p>Correo electronico: {{ $email }}</p>
+      <p>Teléfono: 555-1234</p>
+    </div>
+
+    <div class="receptor">
+      <h2>Información del receptor</h2>
+      <p>Nombre: Ana Gómez</p>
+      <p>Dirección: Calle 456, Ciudad</p>
+      <p>Teléfono: 555-5678</p>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Producto</th>
+          <th>Cantidad</th>
+          <th>Precio unitario</th>
+          <th>Total</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($content as $item)
+        <!-- <h5>{{ $item->name_product }}</h5>
+        <h5>{{ $item->sell_price }}</h5>
+        <h5>{{ $item->content }}</h5>
+        <h5>{{ $item->category->name }}</h5>
+        <h5>{{ $item->brand->name }}</h5> -->
+          <tr>
+            <td>{{ $item->name_product }}</td>
+            <td>{{ $item->quantity }}</td>
+            <td>{{ $item->sell_price }}</td>
+            <td>{{ number_format($item->sell_price * $item->quantity,2)}}</td>
+          </tr>
+        @endforeach
+        
+      </tbody>
+    </table>
+
+    <div class="total">
+      <p>Total: {{ number_format($total,2) }} €</p>
+    </div>
+</body>
+</html>
