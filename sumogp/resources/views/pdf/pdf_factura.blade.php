4c96434e9660a291ac60b51fdccd2b395e7b2bf3
paumotos <pmotos@insdanielblanxart.cat>
2023-05-04 16:19:27 +0200

    pdf

--- sumogp/resources/views/pdf/pdf_factura.blade.php
+++ sumogp/resources/views/pdf/pdf_factura.blade.php
@@ -85,23 +85,50 @@
     <tbody>
       <tr>
         <td>
-          <div class="emisor">
-            <h2>Información de la empresa</h2>
-            <p>Nombre: INK SPOT S.L.L</p>
-            <p>Correo electronico: inkspotsll86@gmial.com</p>
-            <p>Teléfono: 613621145</p>
-            <p>Cif: B-43536274</p>
-            <p>Direccion: C/ Conssell de Cent nº282</p>
-          </div>
+          <tr>
+            <th> <h2>Información de la empresa</h2></th>
+          </tr>
+          <tr>
+            <th><p>Nombre: INK SPOT S.L.L</p></th>
+          </tr>
+          <tr>
+            <th><p>Correo electronico: inkspotsll86@gmial.com</p></th>
+          </tr>
+          <tr>
+            <th> <p>Teléfono: 613621145</p></th>
+          </tr>
+          <tr>
+            <th> <p>Cif: B-43536274</p></th>
+          </tr>
+          <tr>
+            <th> <p>Direccion: C/ Conssell de Cent nº282</p></th>
+          </tr>
+          <!-- <div class="emisor"> 
+          </div> -->
         </td>
         <td>
-          <div class="emisor">
-            <h2>Información del cliente</h2>
-            <p>Nombre: {{ $nameuser }}</p>
-            <p>Apellidos: {{ $last_name }}</p>
-            <p>Correo electronico: {{ $email }}</p>
-            <p>Teléfono: 555-1234</p>
-          </div>
+          <tr>
+            <th> <h2>Información del cliente</h2></th>
+          </tr>
+          <tr>
+            <th><p>Nombre: {{ $nameuser }}</p></th>
+          </tr>
+          <tr>
+            <th><p>Apellidos: {{ $last_name }}</p></th>
+          </tr>
+          <tr>
+            <th><p>Correo electronico: {{ $email }}</p></th>
+          </tr>
+          <tr>
+            <th><p>Teléfono: 555-1234</p></th>
+          </tr>
+          <!-- <div class="emisor">
+           
+            
+            
+            
+            
+          </div> -->
         </td>
       </tr>
     </tbody>
